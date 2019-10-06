0de8cda52cecc37f803b454f1f38a6d1643bc135
saburo ibuki <bean.sproutman3163@gmail.com>
2019-10-06 17:53:49 +0900

    Kaggle first trying
    Qiitaの記事をそのままトレース

--- titanic_prediction/titanic_prediction.py
+++ titanic_prediction/titanic_prediction.py
@@ -1,6 +1,9 @@
 import os
+import csv
+
 import pandas
-import matplotlib.pyplot as PyPlot
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
 
 import definition as Def
 
@@ -16,25 +19,64 @@ os.chdir(os.path.dirname(__file__))
 # Embarked ... 乗船した港 [Cherbourg, Queenstown, Southampton]
 
 # replace male = 0, female = 1
-dataFrame = pandas.read_csv("train.csv").replace("male", Def.MALE).replace("female", Def.FEMALE)
+data_frame = pandas.read_csv("train.csv").replace("male", Def.MALE).replace("female", Def.FEMALE)
 
 # Age が空白のものを抽出 (NaN != NaN)
-emptyAge = dataFrame.query('Age != Age')
+emptyAge = data_frame.query('Age != Age')
 print(emptyAge)
 
 # fill empty data [Age]
-dataFrame["Age"].fillna(dataFrame.Age.median(), inplace=True)
+data_frame["Age"].fillna(data_frame.Age.median(), inplace=True)
 
 splitData = []
 for survived in [Def.DEAD, Def.SURVIVED]:
-	splitData.append(dataFrame[dataFrame.Survived == survived])
+	splitData.append(data_frame[data_frame.Survived == survived])
+plot_data = [i["Pclass"].dropna() for i in splitData]
+
+print(plot_data)
+axes = plt.subplot()
+axes.hist(plot_data, histtype="barstacked", bins=3, cumulative=False, rwidth=0.5, color=['red', 'green'], label=['Dead', 'Survived'])
+axes.set_title('title')
+
+#plt.show()
 
 
-figure = PyPlot.figure()
+data_frame["FamliySIze"] = data_frame["SibSp"] + data_frame["Parch"] + 1
+data_frame_fmt = data_frame.drop(["Name", "SibSp", "Parch", "Ticket", "Fare", "Cabin", "Embarked"], axis=1)
+
+train_data = data_frame_fmt.values
+xs = train_data[:, 2:]
+y = train_data[:, 1]
+
+print("_________XS___________")
+print(xs)
+print("_________Y___________")
+print(y)
+forest = RandomForestClassifier(n_estimators=100)
+forest = forest.fit(xs, y)
+
+test_df = pandas.read_csv("test.csv").replace("male", Def.MALE).replace("female", Def.FEMALE)
+
+# 補完
+test_df["Age"].fillna(data_frame.Age.median(), inplace=True)
+test_df["FamilySize"] = test_df["SibSp"] + test_df["Parch"] + 1
+test_df_fmt = test_df.drop(["Name", "SibSp", "Parch", "Ticket", "Fare", "Cabin", "Embarked"], axis=1)
 
 
-plotData = [i["Pclass"].dropna() for i in splitData]
-PyPlot.hist(plotData, histtype="barstacked", bins=3, cumulative=False, rwidth=0.5, color=['red', 'green'], label=['Dead', 'Survived'])
-PyPlot.show()
+print(test_df_fmt)
+test_data = test_df_fmt.values
+xs_test = test_data[:, 1:]
+print(xs_test)
+output = forest.predict(xs_test)
 
+print(len(test_data[:, 0]), len(output))
+zip_data = zip(test_data[:, 0].astype(int), output.astype(int))
+predict_data = list(zip_data)
+
+import csv
+with open("predict_result_data.csv", "w") as f:
+    writer = csv.writer(f, lineterminator='\n')
+    writer.writerow(["PassengerId", "Survived"])
+    for pid, survived in zip(test_data[:,0].astype(int), output.astype(int)):
+        writer.writerow([pid, survived])
 
